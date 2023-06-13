db0a598f1bcf5d9e9fd086c803992623e67bcc07
FarhanaIslamSaima <farhanachowdhury2152001@gmail.com>
2023-06-13 11:52:48 +0600

    updated

---
+++ Array-2/PowXn.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  
+    double myPow(double x, int n) {
+        long long nn=n;
+        if(n<0){
+            nn=-1*nn;
+        }
+        double ans=1;
+        while(nn>0){
+            if(nn%2==0){
+                x=x*x;
+                nn=nn/2;
+
+            }
+            if(nn%2==1){
+                ans=ans*x;
+                nn=nn-1;
+            }
+        }
+        if(n<0){
+            ans=1/ans;
+        }
+        
+        return ans;
+        
+    }
+};
